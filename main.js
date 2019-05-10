9f9df18d0cf073203a7b953f8eb918a2314868bd
Kai Marshland <kaimarshland@gmail.com>
2019-05-10 00:47:41 -0700

    Catch uncaught exceptions

--- main.js
+++ main.js
@@ -7,4 +7,9 @@ const httpServer = http.createServer();
 httpServer.listen(port);
 console.log(`Listening on port ${port}`);
 
+process.on('uncaughtException', function (err) {
+    console.error(err.stack);
+    console.log("Node NOT Exiting...");
+});
+
 httpServer.on('request', handleHTTPRequest);
